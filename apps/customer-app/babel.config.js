2838a6bc7c067ce94f064755eb4a7d1808294720
gr3enink-stack <gr3enink@gmail.com>
2026-04-08 14:21:31 +0000

    Add @babel/plugin-transform-private-methods for TanStack Query v5 compatibility

--- apps/customer-app/babel.config.js
+++ apps/customer-app/babel.config.js
@@ -12,6 +12,7 @@ module.exports = function(api) {
           },
         },
       ],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
       'react-native-reanimated/plugin',
     ],
   };
